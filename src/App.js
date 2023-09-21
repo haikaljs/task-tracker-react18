1a05e9021c38f80d3e02bec01ba7171f0486db97
haikaljs <haikaljavascript@gmail.com>
2023-09-22 03:32:57 +0800

    Toggle reminder on json-server

--- src/App.js
+++ src/App.js
@@ -24,6 +24,13 @@ const App = () => {
     return data;
   };
 
+  // Fetch single task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   const addTask = async (task) => {
     // const id = Math.floor(Math.random() * 1000) + 1;
     // const newTask = {
@@ -39,7 +46,7 @@ const App = () => {
       body: JSON.stringify(task),
     });
     const data = await res.json();
-    setTasks([...tasks, data])
+    setTasks([...tasks, data]);
   };
 
   // Delete task
@@ -49,10 +56,23 @@ const App = () => {
   };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToggle = await fetchTask(id);
+    const updTask = { ...taskToggle, reminder: !taskToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
