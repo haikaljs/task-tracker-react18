38caab86abfdc13486e2e4a9cb7d46eebb4a1f3e
haikaljs <haikaljavascript@gmail.com>
2023-09-21 23:18:27 +0800

    Pass onClick function as props from header to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click");
+  };
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" />
-      <Button color="red" text="Add" />
-      <Button color="blue" text="Add" />
+      <Button onClick={onClick} color="green" text="Add" />
+      {/* <Button color="red" text="Add" />
+      <Button color="blue" text="Add" /> */}
     </header>
   );
 };
