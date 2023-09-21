8bf27c4b88fad0c585786047563e17c627cb5bd1
haikaljs <haikaljavascript@gmail.com>
2023-09-22 00:12:27 +0800

    Toggle reminder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,10 @@
 import Task from "./Task";
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task task={task} key={task.id} onDelete={onDelete} />
+        <Task task={task} key={task.id} onDelete={onDelete} onToggle={onToggle}/>
       ))}
     </>
   );
