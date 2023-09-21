fcfb5df256c1ed28ceeb561d58a42e795e2cc303
haikaljs <haikaljavascript@gmail.com>
2023-09-21 20:19:51 +0800

    Header components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='hello'/>
     </div>
   );
-}
+};
+
+
+// Example of class base components
+// class App extends React.Component {
+//   render() {
+//     return <h1>Class base components</h1>;
+//   }
+// }
 
 export default App;
