51165627e2b831f984a99a4b920eb7157cf8d1bd
haikaljs <haikaljavascript@gmail.com>
2023-09-21 23:05:39 +0800

    Import styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
+import "./index.css";
 
 const App = () => {
   return (
     <div className="container">
-      <Header title="Hello" />
+      <Header title="Task Tracker" />
     </div>
   );
 };
