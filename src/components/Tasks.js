792f5b3b6a532aa303c6793791504e5274b81955
haikaljs <haikaljavascript@gmail.com>
2023-09-21 23:32:41 +0800

    state and useState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,8 @@
-const tasks = [
+import { useState } from "react";
+
+
+const Tasks = () => {
+const [tasks, setTasks] = useState([
   { id: 1, text: "Learn PHP", day: "Feb 5th at 2:30pm", reminder: true },
   { id: 2, text: "Learn Laravel", day: "Feb 6th at 3:30pm", reminder: true },
   {
@@ -9,9 +13,8 @@ const tasks = [
   },
   { id: 4, text: "Learn Reactjs", day: "Feb 8th at 5:30pm", reminder: false },
   { id: 5, text: "Learn Vuejs", day: "Feb 9th at 6:30pm", reminder: false },
-];
-
-const Tasks = () => {
+]
+)
   return (
     <>
       {tasks.map((task) => (
