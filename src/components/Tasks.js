2132416b079022f7d8a833ec0065cb6671020a56
haikaljs <haikaljavascript@gmail.com>
2023-09-21 23:23:07 +0800

    Create Task component

---
+++ src/components/Tasks.js
@@ -0,0 +1,21 @@
+const tasks = [
+  { id: 1, text: "Learn PHP", day: "Feb 5th at 2:30pm", reminder: true },
+  { id: 2, text: "Learn Laravel", day: "Feb 6th at 3:30pm", reminder: true },
+  {
+    id: 3,
+    text: "Learn Javascript",
+    day: "Feb 7th at 4:30pm",
+    reminder: false,
+  },
+  { id: 4, text: "Learn Reactjs", day: "Feb 8th at 5:30pm", reminder: false },
+  { id: 5, text: "Learn Vuejs", day: "Feb 9th at 6:30pm", reminder: false },
+];
+
+
+const Tasks = () => {
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
