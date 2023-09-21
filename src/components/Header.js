aa837fd1288e58e34f6cf8dfec16f9d9006659d7
haikaljs <haikaljavascript@gmail.com>
2023-09-21 23:13:18 +0800

    Button components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" />
+      <Button color="red" text="Add" />
+      <Button color="blue" text="Add" />
     </header>
   );
 };
