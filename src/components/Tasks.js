252db5a566c958ceaf081903d89065a2cef98008
haikaljs <haikaljavascript@gmail.com>
2023-09-21 23:36:06 +0800

    Global state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,20 +1,4 @@
-import { useState } from "react";
-
-
-const Tasks = () => {
-const [tasks, setTasks] = useState([
-  { id: 1, text: "Learn PHP", day: "Feb 5th at 2:30pm", reminder: true },
-  { id: 2, text: "Learn Laravel", day: "Feb 6th at 3:30pm", reminder: true },
-  {
-    id: 3,
-    text: "Learn Javascript",
-    day: "Feb 7th at 4:30pm",
-    reminder: false,
-  },
-  { id: 4, text: "Learn Reactjs", day: "Feb 8th at 5:30pm", reminder: false },
-  { id: 5, text: "Learn Vuejs", day: "Feb 9th at 6:30pm", reminder: false },
-]
-)
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
