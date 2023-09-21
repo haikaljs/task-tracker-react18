16cd65cff6cd0430a219a2061d37758c793dc188
haikaljs <haikaljavascript@gmail.com>
2023-09-22 03:06:37 +0800

    Add task to json-server

--- src/App.js
+++ src/App.js
@@ -24,13 +24,22 @@ const App = () => {
     return data;
   };
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = {
-      id,
-      ...task,
-    };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // const newTask = {
+    //   id,
+    //   ...task,
+    // };
+    // setTasks([...tasks, newTask]);
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTasks([...tasks, data])
   };
 
   // Delete task
